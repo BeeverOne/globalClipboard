99e145f3b493c1d85b8056159589c7c42a265287
Oluwafemi Bamigboye <oluwafemibamigboye@gmail.com>
2025-01-16 14:10:26 +0100

    RESET commit. Electron course not going as hoped. New Electron app has been added to the electron apps folder

--- electron-apps/electron-textEditor/main.js
+++ electron-apps/electron-textEditor/main.js
@@ -1,8 +1,32 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain, dialog } = electron;
+const path = require("path");
+const fs = require("fs");
+const { error, log } = require("console");
 
+let win;
 //What to do when the app is ready
 app.on("ready", () => {
-  let win = new BrowserWindow({});
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, "./preload.js"),
+    },
+  });
   win.loadFile("./index.html");
+});
+
+ipcMain.on("save", (event, text) => {
+  console.log(text);
+  dialog.showSaveDialog(win, { defaultPath: "filename.txt" }, (fullPath) => {
+    if (fullPath) {
+      fs.writeFile(fullPath, text, (err) => {
+        if (err) {
+          console.log("There was an error", err);
+        } else {
+          console.log("File has been saved");
+        }
+      });
+    }
+  });
 });
